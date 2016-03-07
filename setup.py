45bd200e1e4c45d861d576baefb028e50ce14c66
Aly Merchant <aly.merchant@ec.gc.ca>
2016-03-06 20:30:46 -0500

    feat(registration): add metadata parsing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="rcs",
     description="RAMP Configuration Service",
-    version="2.0.0-1",
+    version="2.0.0-2",
     author="Environment Canada",
     author_email="mike.weech@ec.gc.ca",
     url="https://github.com/fgpv-vpgf/rcs",
